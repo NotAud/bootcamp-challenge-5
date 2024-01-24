f06cd3361417c9c7921e6596bda9e0caaf25f120
Ron <rmiller232501@gmail.com>
2024-01-23 19:18:18 -0500

    Clean up todo comments

--- Develop/script.js
+++ Develop/script.js
@@ -109,12 +109,6 @@ $(function () {
     localStorage.setItem("schedule", JSON.stringify(schedule));
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   // Format date object to display expected output (based on locale)
   // Reuse our date object
   const formattedDate = currentDate.toLocaleDateString("default", {
